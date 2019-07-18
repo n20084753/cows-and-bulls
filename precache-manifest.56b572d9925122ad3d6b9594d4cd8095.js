244d13be24f35a7f5a9c60b1c5124a917535eb57
Rajeshkumar Animuthu <n20084753>
2019-07-18 11:32:25 +0530

    Updates

---
+++ precache-manifest.56b572d9925122ad3d6b9594d4cd8095.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb528e0617fa1af5704a92ba326dbb41",
+    "url": "/cows-and-bulls/index.html"
+  },
+  {
+    "revision": "72b89f547019c2edb1cf",
+    "url": "/cows-and-bulls/static/css/main.5a95a5f5.chunk.css"
+  },
+  {
+    "revision": "115e6343c187827c94dc",
+    "url": "/cows-and-bulls/static/js/2.60db00ca.chunk.js"
+  },
+  {
+    "revision": "72b89f547019c2edb1cf",
+    "url": "/cows-and-bulls/static/js/main.f917bf7f.chunk.js"
+  },
+  {
+    "revision": "b020c70d715c78ec8d25",
+    "url": "/cows-and-bulls/static/js/runtime~main.390ba036.js"
+  },
+  {
+    "revision": "96c3ef1a1de59c0b4419a3b845597d96",
+    "url": "/cows-and-bulls/static/media/cow.96c3ef1a.png"
+  },
+  {
+    "revision": "42cfc1a05b1c2e3cef702efb783fc051",
+    "url": "/cows-and-bulls/static/media/header-logo.42cfc1a0.png"
+  },
+  {
+    "revision": "09e6afa1709a42daa1bed954d101af86",
+    "url": "/cows-and-bulls/static/media/wrong.09e6afa1.png"
+  }
+]);
